cc213ba35adc6e7e340a7992359568a7d9fc2dae
chensxb97 <65479937+chensxb97@users.noreply.github.com>
2021-03-15 21:38:20 +0800

    Non-optimized implementation

--- search.py
+++ search.py
@@ -43,83 +43,122 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     # Open queries file
     queries = open(queries_file, 'r')
 
-    # Process queries ()
-    # query_array = [{word: word frequency in query1},{word: word frequency in query2}...]
-    query_array = []
-    for line in queries:
-        for sentence in nltk.sent_tokenize(line):
-            query_dict = {}
-            for word in nltk.word_tokenize(sentence):
-                word = word.lower()
-                word = stemmer.stem(word)
-                if word in query_dict.keys():
-                    query_dict[word] += 1
-                else:
-                    query_dict[word] = 1
-            # Denominator for normalization of tf-idf (query)
-            normalize_query = 0
-            for word in query_dict.keys():
+    # Store the results for each query
+    # query_results = [[result for query1],[result for query 2]...]
+    query_results = []
+    for query in queries:
+        for sentence in nltk.sent_tokenize(query):
+            # Store all normalized query tf-idf weights in query_dict
+            query_dict = process_query(sentence,sorted_index_dict,collection_size,stemmer)
+            #print('QUERY DICTIONARY\n',query_dict)
+
+            # Store all normalized document tf weights in document_dict
+            document_dict = process_documents(query_dict,sorted_index_dict,docLengths_dict,postings)
+            #print('DOCUMENT DICTIONARY\n',document_dict)
+
+            # Generates the top 10 documents for the query
+            scores = process_scores(query_dict,document_dict)
+
+            query_results.append(scores)
+
+    # Write results into given results_file
+    with open(results_file, 'w') as results_file:
+        for result in query_results:
+            for index, (docID,score) in enumerate(result):
+                results_file.write(docID)
+                results_file.write(' ')
+            results_file.write('\n')
+
+    print('done!')
+
+def process_query(input_query,sorted_index_dict,collection_size,stemmer):
+    query_dict = {}
+    for word in nltk.word_tokenize(input_query):
+        word = word.lower()
+        word = stemmer.stem(word)
+        if word in query_dict.keys():
+            query_dict[word] += 1
+        else:
+            query_dict[word] = 1
+    # Denominator for normalization of tf-idf (query)
+    normalize_query = 0
+    for word in query_dict.keys():
+        # Calculate tf-wt
+        q_tf = query_dict[word]
+        q_tf_wt = 1 + math.log10(q_tf)
+        # Calculate idf
+        if word in sorted_index_dict.keys():
+            q_df = sorted_index_dict[word][1]
+            q_idf = math.log10(collection_size/q_df)
+        else:
+            q_idf = 0
+        q_wt = q_tf_wt * q_idf
+        # Store wt for each word in query back into dictionary
+        query_dict[word] = q_wt
+        normalize_query += math.pow(q_wt, 2)
+    if normalize_query ==0:
+        return None
+    # Calculate the cosine normalized value for query
+    for word in query_dict.keys():
+        q_wt = query_dict[word]
+        normalize_wt = q_wt/math.sqrt(normalize_query)
+        query_dict[word] = normalize_wt
+
+    return query_dict
+
+def process_documents(query_dictionary,sorted_index_dict, docLengths_dict,input_postings):
+    # document_dict = {document1: {word1: tf1, word2: tf2}, document2:{}...}
+    if query_dictionary ==None:
+        return None
+    document_dict = {}
+    for word in query_dictionary.keys():
+        if word in sorted_index_dict.keys():
+            charOffset = sorted_index_dict[word][2]
+            strLength = sorted_index_dict[word][3]
+            input_postings.seek(charOffset,0)
+            posting_str = (input_postings.read(strLength))
+            posting_array = posting_str.split(',')
+            for p in posting_array:
+                documentID = p.split('^')[0]
+                tf_raw = p.split('^')[1]
+                if documentID not in document_dict.keys():
+                    document_dict[documentID] = {}
+                document_dict[documentID][word] = int(tf_raw)
+        else:
+            pass
+    for document in document_dict.keys():
+        # Denominator for normalization of tf-idf (document)
+        normalize_doc = docLengths_dict[int(document)]
+        for word in query_dictionary.keys():
+            if word in document_dict[document].keys():
+                d_tf = int(document_dict[document][word])
                 # Calculate tf-wt
-                q_tf = query_dict[word]
-                q_tf_wt = 1 + math.log10(q_tf)
-                # Calculate idf
-                if word in sorted_index_dict.keys():
-                    q_df = sorted_index_dict[word][1]
-                    q_idf = math.log10(collection_size/q_df)
-                else:
-                    idf = 0
-                q_wt = q_tf_wt * q_idf
-                # Store wt for each word in query back into dictionary
-                query_dict[word] = q_wt
-                normalize_query += math.pow(q_wt, 2)
-            # Calculate the cosine normalized value for query tf-idf
-            for word in query_dict.keys():
-                q_wt = query_dict[word]
-                normalize_wt = q_wt/math.sqrt(normalize_query)
-                query_dict[word] = normalize_wt
+                d_tf_wt = 1 + math.log10(d_tf)
+                # Store tf-wt for each word in document back into dictionary
+                document_dict[document][word] = d_tf_wt
+            else:
+                document_dict[document][word] = 0
+        for word in query_dictionary.keys():
+            # Calculate the cosine normalized value for each document
+            d_wt = document_dict[document][word]
+            d_normalize_wt = d_wt/normalize_doc
+            document_dict[document][word] = d_normalize_wt
 
-            print('QUERY DICTIONARY\n',query_dict)
+    return document_dict
 
-            # Parse postings list for each word for valid documents which contains these words
-            # Each document we will have an array of numbers for each word in the query
-            # document_dict = {document1: {word1: tf1, word2: tf2}, document2:{}...} 
-            document_dict = {}
-            for word in query_dict.keys():
-                if word in sorted_index_dict.keys():
-                    termID = sorted_index_dict[word][0]
-                    charOffset = sorted_index_dict[word][2]
-                    strLength = sorted_index_dict[word][3]
-                    postings.seek(charOffset, 0)
-                    posting_str = (postings.read(strLength))
-                    posting_array = posting_str.split(',')
-                    for p in posting_array:
-                        documentID = p.split('^')[0]
-                        tf_raw = p.split('^')[1]
-                        document_dict[documentID] = {}
-                        document_dict[documentID][word] = tf_raw
-                else:
-                    pass
-            # Calculate the cosine normalized value for document tf-idf
-            for document in document_dict.keys():
-                # Denominator for normalization of tf-idf (document)
-                normalize_doc = docLengths_dict[int(document)]
-                print('LENGTH FOR DOC {}: {}'.format(document,normalize_doc))
-                for word in query_dict.keys():
-                    if word in document_dict[document].keys():
-                        d_tf = int(document_dict[document][word])
-                        d_tf_wt = 1 + math.log10(d_tf)
-                        document_dict[document][word] = d_tf_wt
-                    else:
-                        document_dict[document][word] = 0
-                for word in query_dict.keys():
-                    d_wt = document_dict[document][word]
-                    d_normalize_wt = d_wt/normalize_doc
-                    document_dict[document][word] = d_normalize_wt
-
-            print('DOCUMENT DICTIONARY\n',document_dict)
-
-            query_array.append(query_dict)
-
+def process_scores(query_dictionary,document_dictionary):
+    if query_dictionary== None:
+        return None
+    result = []
+    for docID in document_dictionary.keys():
+        docScore = 0
+        for term in query_dictionary.keys():
+            doc_wt = document_dictionary[docID][term]
+            term_wt = query_dictionary[term]
+            docScore+=doc_wt*term_wt
+        result.append((docID,docScore))
+    sorted_results = sorted(result, key=lambda x: x[1],reverse = True)
+    return sorted_results[:10]
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
